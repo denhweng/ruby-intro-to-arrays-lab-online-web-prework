4dbc2d200b727326323fec4dd915978807920e12
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-08 07:13:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -32,3 +32,6 @@ def last_element_with_array_methods(south_east_asia)
   return south_east_asia.last
 end
 
+programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+
+def 
