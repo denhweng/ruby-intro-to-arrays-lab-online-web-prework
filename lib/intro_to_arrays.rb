d1240816446c874c76b2be8a0650ce6a7e2707f8
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-08 07:02:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,17 +8,16 @@ def array_with_two_elements
   return array 
 end
 
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+
 def first_element(taylor_swift)
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   return taylor_swift(0)
 end
 
 def third_element(taylor_swift)
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   return taylor_swift(2)
 end
 
 def last_element(taylor_swift)
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   return taylor_swift(-1)
 end
